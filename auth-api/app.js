09215533810c982c363b88a4153199261923d6ad
JoelSchecheleski <joel.jsp@gmail.com>
2022-05-06 12:05:36 -0300

    10 - Criado dados iniciais para módulo de usuário

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,8 +1,11 @@
 import express from "express";
+import * as db from "./src/config/db/initialData.js"; // importa funções do módulo
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
+
+db.createInitialData();
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
