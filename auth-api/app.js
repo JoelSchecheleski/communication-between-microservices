56fcde9c6ce77ecd7ed93dae913053fab43d4865
JoelSchecheleski <joel.jsp@gmail.com>
2022-05-30 01:01:56 -0300

    #12 - Adicionado middleware de autenticação

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,15 +1,13 @@
 import express from "express";
 import * as db from "./src/config/db/initialData.js"; // importa funções do módulo
 import userRouter from "./src/modules/router/UserRouter.js";
+import checkToken from "./src/config/auth/checkToken.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
 db.createInitialData();
-
-app.use(express.json());
-app.use(userRouter);
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
@@ -18,6 +16,12 @@ app.get('/api/status', (req, res) => {
         httpStatus: 200
     });
 });
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true} ));
+app.use(userRouter);
+
+app.use(checkToken);
 
 app.listen(PORT, () => {
     console.info(`Server started successfully at port ${PORT}`);
