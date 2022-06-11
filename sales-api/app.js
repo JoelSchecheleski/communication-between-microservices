a9997890209c6ac1809cfdb61b76f519c67cb19d
JoelSchecheleski <joel.jsp@gmail.com>
2022-06-11 09:39:29 -0300

    #30 - Configurado MongoDB e adicionado model Order

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,10 +1,19 @@
 import express from "express";
 
+import {connect} from './src/config/db/mongoDbConfig.js'
+import Order from "./src/modules/sales/model/Order.js";
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8081;
 
+connect();
+
 app.get('/api/status', (req, res) => {
+    let test = Order.find();
+    console.log(test);
+
+
     return res.status(200).json({
         service: 'Sales-api',
         status: 'up',
