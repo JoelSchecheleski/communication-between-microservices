2e19d96599c5876ad99b3515c6919a611fb59a51
JoelSchecheleski <joel.jsp@gmail.com>
2022-06-22 14:39:20 -0300

    31 - Configurado interceptor para validação de token JWT, RAbbitMQ, Listeners, Sender, HTTP REst para Product-API com Axios, controllers e services

--- sales-api/src/config/secrets/secrets.js
+++ sales-api/src/config/secrets/secrets.js
@@ -1,2 +1,12 @@
-const env = process.env;
-export const MONGO_DB_URL =  env.MONGO_DB_URL ? MONGO_DB_URL : "mongodb://localhost:27017/sales-db";
+// import config from 'dotenv'
+// // const env = process.env;
+
+// console.log(process.env)
+
+// MONGO_USER = process.env.MONGO_USER = 'root';
+// console.info('MONGO_USER: ', MONGO_USER);
+
+// // export const MONGO_USER = env.MONGO_USER ? env.MONGO_USER : "root";
+// export const MONGO_PASSWORD = env.MONGO_PASSWORD ? env.MONGO_PASSWORD : "xOawerLu7";
+// export const MONGO_DB_URL =  env.MONGO_DB_URL ? env.MONGO_DB_URL : "mongodb://localhost/sales-db?retryWrites=true&w=majority";
+
